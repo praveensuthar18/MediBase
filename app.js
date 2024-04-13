6f0b6fdc7185a15b8d829ca6bc6f123910711e2d
Venkata Sai Manoj Pogadadanda <vpogadad@sundevils.asu.edu>
2024-04-12 22:22:54 -0700

    UI Changes

--- app.js
+++ app.js
@@ -6,46 +6,56 @@ searchButton.addEventListener("click", async function () {
   const searchTerm = searchInput.value.toLowerCase();
   //   const response = await fetch(`/search?term=${searchTerm}`);
   //   const data = await response.json();
+  searchResults.style.display = 'none';
 
-  let data = [
-    {
-      title: "Test 3",
-      url: "https...",
-      content: "test",
-      comments: ["comment 1"],
-    },
-  ];
+  var loader = document.getElementById('loader');
+  loader.style.display = 'block';
+  setTimeout(function() { 
+    loader.style.display = 'none';  
+    searchResults.style.display = 'block';
 
-  searchResults.innerHTML = "";
-  data.forEach((post) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const title = document.createElement("h2");
-    title.textContent = post.title;
-
-    const content = document.createElement("p");
-    content.textContent = post.content;
-
-    const url = document.createElement("a");
-    url.textContent = "Read more";
-    url.href = post.url;
-    url.target = "_blank";
-
-    const comments = document.createElement("div");
-    comments.classList.add("comments");
-    comments.innerHTML = "<h3>Comments</h3>";
-    post.comments.forEach((comment) => {
-      const commentText = document.createElement("p");
-      commentText.textContent = comment;
-      comments.appendChild(commentText);
+    let data = [
+      {
+        title: "Test 3",
+        url: "https...",
+        content: "test",
+        comments: ["comment 1"],
+      },
+    ];
+  
+    searchResults.innerHTML = "";
+    data.forEach((post) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+      const title = document.createElement("h2");
+      title.textContent = post.title;
+  
+      const content = document.createElement("p");
+      content.textContent = post.content;
+  
+      const url = document.createElement("a");
+      url.textContent = "Read more";
+      url.href = post.url;
+      url.target = "_blank";
+  
+      const comments = document.createElement("div");
+      comments.classList.add("comments");
+      comments.innerHTML = "<h3>Comments</h3>";
+      post.comments.forEach((comment) => {
+        const commentText = document.createElement("p");
+        commentText.textContent = comment;
+        comments.appendChild(commentText);
+      });
+  
+      card.appendChild(title);
+      card.appendChild(content);
+      card.appendChild(url);
+      card.appendChild(comments);
+  
+      searchResults.appendChild(card);
     });
+  
+  }, 500);
 
-    card.appendChild(title);
-    card.appendChild(content);
-    card.appendChild(url);
-    card.appendChild(comments);
-
-    searchResults.appendChild(card);
-  });
 });
