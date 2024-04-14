5ee5c19b55ccc97921f53fb12c0dad261eb4e0fd
Gaurav <kulkarni.gaurav97@gmail.com>
2024-04-13 18:50:15 -0700

    Updated server

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
 const searchResults = document.getElementById("searchResults");
+const symptomsContainer = document.getElementById("symptomsContainer");
+const addSymptoms = document.getElementById("addSymptoms");
+const addSymptomButton = document.getElementById("addSymptomButton");
+const symptomsDiv = document.getElementById("add-symptoms")
 
 searchButton.addEventListener("click", async function () {
+  symptomsDiv.style.visibility = "visible"
   const searchTerm = searchInput.value.toLowerCase();
-  //   const response = await fetch(`/search?term=${searchTerm}`);
-  //   const data = await response.json();
-  searchResults.style.display = "none";
 
-  const symptomsContainer = document.getElementById("symptomsContainer");
+  const requestBody = {
+    query: searchTerm
+  };
+  
+  const response = await fetch('http://localhost:8080/process-query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  });
+  
+  const data = await response.json();
+  searchResults.style.display = "none";
   symptomsContainer.innerHTML = "";
 
-  data = {
-    symptoms: ["cough", "headache", "pain"],
-  };
+  addSymptomButton.addEventListener("click", async () => {
+    if(addSymptoms.value != "") {
+      drawPill(addSymptoms.value.toLowerCase(), true)
+    }
+    addSymptoms.value = ""
 
+  })
   data.symptoms.forEach((symptom) => {
-    const pill = document.createElement("div");
-    pill.classList.add("pill");
-
-    pill.textContent = symptom;
-
-    // Set the width of the pill based on the text content
-    const width = pill.textContent.length * 12; // Adjust the multiplier as needed
-    pill.style.width = `${width}px`;
-
-    pill.addEventListener("click", () => {
-      // Toggle selection
-      pill.classList.toggle("selected");
-
-      // Show the 'Get Posts' button if at least one symptom is selected
-      const selectedSymptoms = document.querySelectorAll(".selected");
-      const getPostsButton = document.getElementById("getPostsButton");
-      getPostsButton.style.display =
-        selectedSymptoms.length > 0 ? "block" : "none";
-    });
-    symptomsContainer.appendChild(pill);
+    drawPill(symptom)
   });
+
+
+
 });
 
 document
@@ -47,74 +49,48 @@ document
       (symptom) => symptom.textContent
     );
 
-    // // Send a request to get posts containing selected symptoms
-    // const response = await fetch(
-    //   `your_api_url/posts?symptoms=${JSON.stringify(symptomNames)}`
-    // );
-    // const data = await response.json();
-
+    const requestBody = {
+      query: symptomNames
+    };
+    
+    const response = await fetch('http://localhost:8080/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    });
+    
+    const data = await response.json();
+    
     var loader = document.getElementById("loader");
     loader.style.display = "block";
     setTimeout(function () {
       loader.style.display = "none";
       searchResults.style.display = "block";
 
-      let data = [
-        {
-          title: ["Test 3"],
-          url: [
-            "www.medhelp.org/posts/Gallbladder/Sphincter-of-oddi-dysfunction-is-ruling-my-life-please-hel/show/142363",
-          ],
-          content: ["test"],
-          comments: ["comment 1"],
-          symptoms: [
-            "Stomach ache",
-            "Abdominal Pain",
-            "Pain",
-            "Vomiting",
-            "Nausea",
-            "back pain mid back",
-          ],
-          diagnostic_procedures: [
-            "Laser-Induced Fluorescence Endoscopy",
-            "Radionuclide Imaging",
-            "Cholangiopancreatography, Magnetic Resonance",
-            "Scanning",
-            "Ultrasonography",
-            "Endoscopic Retrograde Cholangiopancreatography",
-          ],
-
-          diseases: [
-            "FANCONI ANEMIA, COMPLEMENTATION GROUP E",
-            "Abetalipoproteinemia",
-            "Endometriosis",
-            "Brachial Amyotrophic Diplegia",
-            "Liver diseases",
-            "Stomach Diseases",
-          ],
-        },
-      ];
-
       searchResults.innerHTML = "";
       data.forEach((post) => {
+        console.log(post)
         const card = document.createElement("div");
         card.classList.add("card");
 
-        const title = document.createElement("h2");
-        title.textContent = post.title[0];
+        // const title = document.createElement("h2");
+        // title.textContent = post.post_content[0];
 
         const content = document.createElement("p");
-        content.textContent = post.content[0];
+        content.textContent = post.post_content[0];
 
         const url = document.createElement("a");
         url.textContent = "Read more";
-        url.href = `https://${post.url[0]}`;
+        url.href = `https://${post.post_url[0].trim()}`;
         url.target = "_blank";
+        console.log(url)
 
         const comments = document.createElement("div");
         comments.classList.add("comments");
         comments.innerHTML = "<h3>Comments</h3>";
-        post.comments.forEach((comment) => {
+        post.post_comments.forEach((comment) => {
           const commentText = document.createElement("p");
           commentText.textContent = comment;
           comments.appendChild(commentText);
@@ -150,7 +126,7 @@ document
           diagnosticProcedures.appendChild(procedurePill);
         });
 
-        card.appendChild(title);
+        // card.appendChild(title);
         card.appendChild(content);
         card.appendChild(url);
         card.appendChild(symptoms);
@@ -162,3 +138,30 @@ document
       });
     }, 500);
   });
+
+function drawPill(symptom, selected = false) {
+  const pill = document.createElement("div");
+    pill.classList.add("pill");
+
+    pill.textContent = symptom;
+
+    // Set the width of the pill based on the text content
+    const width = pill.textContent.length * 12; // Adjust the multiplier as needed
+    pill.style.width = `${width}px`;
+    if(selected) {
+      pill.classList.toggle("selected");
+      document.getElementById("getPostsButton").style.display = "block";
+    }
+
+    pill.addEventListener("click", () => {
+      // Toggle selection
+      pill.classList.toggle("selected");
+
+      // Show the 'Get Posts' button if at least one symptom is selected
+      const selectedSymptoms = document.querySelectorAll(".selected");
+      const getPostsButton = document.getElementById("getPostsButton");
+      getPostsButton.style.display =
+        selectedSymptoms.length > 0 ? "block" : "none";
+    });
+    symptomsContainer.appendChild(pill);
+}
